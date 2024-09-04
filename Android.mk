5837624ec80ffd1f5cb1a7d2873281465f0f5cf1
GioKas93 <giokas93@pm.me>
2024-09-04 22:07:33 +0200

    fixup! copy headers to KERNEL_OBJ

--- Android.mk
+++ Android.mk
@@ -1,5 +1,5 @@
 # Kernel headers
-$(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr: $(wildcard device/xiaomi/nuwa-kernel/kernel-headers/*)
+$(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr: $(wildcard device/xiaomi/vermeer-kernel/kernel-headers/*)
 	rm -rf $@
 	mkdir -p $@/include
-	cp -a device/xiaomi/nuwa-kernel/kernel-headers/. $@/include
+	cp -a device/xiaomi/vermeer-kernel/kernel-headers/. $@/include
